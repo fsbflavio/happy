303c18c5cd3c0b4c2a2b05ce957c2a0f0d89f736
flavio <flavio.baia@pronetsoftware.com.br>
2020-10-15 12:58:02 -0300

    migration

---
+++ backend/src/database/migrations/1602777450676-create_image.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createImage1602777450676 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
